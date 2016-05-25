aeb37afe60b0b396e41b85c964d24e98edba533d
Rehan Mahmood <rehanone@gmail.com>
2016-05-24 20:44:09 -0400

    Fix tests

--- spec/classes/nginx_spec.rb
+++ spec/classes/nginx_spec.rb
@@ -11,10 +11,10 @@ describe 'nginx' do
     it { is_expected.to compile.with_all_deps }
     it { is_expected.to contain_class('nginx') }
     it { is_expected.to contain_anchor('nginx::begin') }
-    it { is_expected.to contain_nginx__repo.that_requires('Anchor[nginx::begin]') }
-    it { is_expected.to contain_nginx__config.that_requires('Class[nginx::repo]') }
-    it { is_expected.to contain_nginx__service.that_subscribes_to('Class[nginx::config]') }
-    it { is_expected.to contain_nginx__firewall.that_requires('Class[nginx::service]') }
-    it { is_expected.to contain_anchor('nginx::end').that_requires('Class[nginx::service]') }
+    it { is_expected.to contain_nginx__repo }
+    it { is_expected.to contain_nginx__config }
+    it { is_expected.to contain_nginx__service }
+    it { is_expected.to contain_nginx__firewall }
+    it { is_expected.to contain_anchor('nginx::end') }
   end
 end
