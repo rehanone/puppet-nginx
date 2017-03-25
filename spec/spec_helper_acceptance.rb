16ac172e2db27ec5c635b81d78139da839b14442
Rehan Mahmood <rehanone@gmail.com>
2017-03-25 15:13:56 -0400

    Improve spec_helper. Add Archlinux to supported platforms.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,31 +3,16 @@ require 'beaker/puppet_install_helper'
 
 UNSUPPORTED_PLATFORMS = ['windows']
 
-run_puppet_install_helper
+unless ENV['RS_PROVISION'] == 'no' or ENV['BEAKER_provision'] == 'no'
 
-# This method allows a block to be passed in and if an exception is raised
-# that matches the 'error_matcher' matcher, the block will wait a set number
-# of seconds before retrying.
-# Params:
-# - max_retry_count - Max number of retries
-# - retry_wait_interval_secs - Number of seconds to wait before retry
-# - error_matcher - Matcher which the exception raised must match to allow retry
-# Example Usage:
-# retry_on_error_matching(3, 5, /OpenGPG Error/) do
-#   apply_manifest(pp, :catch_failures => true)
-# end
-def retry_on_error_matching(max_retry_count = 3, retry_wait_interval_secs = 5, error_matcher = nil)
-  try = 0
-  begin
-    try += 1
-    yield
-  rescue Exception => e
-    if try < max_retry_count && (error_matcher == nil || e.message =~ error_matcher)
-      sleep retry_wait_interval_secs
-      retry
-    else
-      raise
-    end
+  run_puppet_install_helper
+
+  hosts.each do |host|
+    environmentpath = host.puppet['environmentpath']
+    environmentpath = environmentpath.split(':').first if environmentpath
+
+    on host, puppet('module install puppetlabs-stdlib --version 4.16.0')
+    on host, puppet('module install puppetlabs-apt --version 2.3.0')
   end
 end
 
@@ -46,8 +31,6 @@ RSpec.configure do |c|
       apply_manifest_on(host, 'package { "software-properties-common": }')
       copy_module_to(host, :source => proj_root, :module_name => 'nginx')
       shell("/bin/touch #{default['puppetpath']}/hiera.yaml")
-      on host, puppet('module install puppetlabs-stdlib --version 4.14.0')
-      on host, puppet('module install puppetlabs-apt --version 2.3.0')
     end
   end
 end
