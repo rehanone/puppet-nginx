0920f491d85cb779314808d5614543c69e016ccc
c33s <dev@c33s.net>
2021-12-03 03:32:52 +0100

    allow to set nginx.conf template via class parameter refs: #31

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,14 +1,19 @@
-class nginx::config () inherits nginx {
+class nginx::config (
+  String $template = "${module_name}/nginx.conf.epp",
+  Hash $template_parameters = {},
+) inherits nginx {
 
   assert_private("Use of private class ${name} by ${caller_module_name}")
 
+  $default_template_parameters = {
+    'install_location' => $nginx::install_location,
+  }
+
+  $_template_parameters = $default_template_parameters + $template_parameters
+
   file { "${nginx::install_location}/nginx.conf":
     ensure  => file,
-    content => epp("${module_name}/nginx.conf.epp",
-      {
-        'install_location' => $nginx::install_location,
-      }
-    ),
+    content => epp($template, $_template_parameters),
   }
   -> file { "${nginx::install_location}/sites-available":
     ensure  => directory,
